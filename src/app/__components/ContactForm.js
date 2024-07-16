71c32496540cabbc0ab77a8a6f4218859d9bcb20
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-16 10:00:03 +0530

    Contact page is modified and integrated with IntroPage

--- src/app/__components/ContactForm.js
+++ src/app/__components/ContactForm.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function ContactForm() {
-  return (
-    <div className=''>
-      Contact form
-    </div>
-  )
+    return (
+        <div className='fixed top-0 left-0 h-[100vh] w-full bg-gray-200 flex items-center justify-center'>
+            <from className="flex flex-col gap-10 bg-white shadow-[0_0_5px_gray] rounded-md w-[600px] p-6">
+                <h2 className="text-[1.7rem] font-bold font-sans text-gray-700">Talk to us</h2>
+                <input type="email" placeholder='Work email' className='outline-none py-3 bg-transparent' />
+                <div className="flex gap-3 items-center justify-between">
+                    <input type="text" placeholder="First name" className='outline-none py-3 bg-transparent w-1/2' />
+                    <input type="text" placeholder="Last name" className='outline-none py-3 bg-transparent w-1/2' />
+                </div>
+                <div className="flex gap-4 items-center justify-center">
+                    <input type="checkbox" className='rounded-full bg-red-300' />
+                    <label htmlFor="" className="text-gray-500 font-sans">I agree to Fyle's terms and conditions, and provide consent to send me communication</label>
+                </div>
+                <button className='py-4 px-5 bg-rose-600 text-white font-semibold rounded-md'>Contact us</button>
+            </from>
+        </div>
+    )
 }
--- src/app/__components/IntroPage.js
+++ src/app/__components/IntroPage.js
@@ -1,15 +1,20 @@
 "use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
+import ContactForm from './ContactForm';
 
 export default function IntroPage() {
+    const [contact , setContact] = useState(false);
   return (
     <div className='w-full h-[100vh] flex gap-10 items-center justify-center'>
       <div className="flex flex-col gap-8 max-w-[40%]">
         <div className="text-[1.3rem] font-medium text-rose-600">AWARD WINNING</div>
         <div className="text-[3rem] font-bold font-sans leading-[3.5rem]">DIGITAL MARKETING AGENCY</div>
         <div className="text-[12px] font-semibold font-sans leading-[1.4rem]">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam consequatur debitis quaerat porro nostrum voluptas error. Voluptas, incidunt odio. Dolorum, nemo? Sunt em magni maiores.</div>
-        <button className="w-fit bg-red-700/90 hover:bg-red-600 text-white rounded py-3 px-8 text-[10px] font-semibold font-sans">CONTACT US</button>
+        <button className="w-fit bg-red-700/90 hover:bg-red-600 text-white rounded py-3 px-8 text-[10px] font-semibold font-sans" onClick={e => setContact(true)}>CONTACT US</button>
+        {
+            contact && <ContactForm setContact={setContact} />
+        }
       </div>
       <div className="">
         <Image src={'/intropage1.png'} alt='symbol' height={600} width={600} className='h-[70vh] w-fit' />
