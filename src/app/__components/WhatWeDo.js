04802c63e6beeab57895e8402144caf2bda387b7
abhis12321 <abhisheksingh2359328@gmail.com>
2024-07-16 12:01:13 +0530

    some UI modified along with the slying in WhatWeDo.js and integrated with home page

--- src/app/__components/WhatWeDo.js
+++ src/app/__components/WhatWeDo.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function WhatWeDo() {
   return (
-    <div className=''>
-      what we do here
+    <div className='h-[100vh] w-full bg-gray-500/5 flex items-center justify-center'>
+        <div className="w-[98%] max-w-[850px] flex flex-col gap-4 items-start justify-center">
+            <div className="text-[1.3rem] font-medium text-rose-600">WHAT WE DO</div>
+            <div className="flex gap-8 items-center justify-center">
+                <div className="text-[2rem] font-bold w-[710px] font-sans leading-[2.5rem]">SERVICES PROVIDE FOR YOU</div>
+                <div className="text-[10px] text-gray-500 font-semibold leading-[1.2rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sit, velit excepturi ea cum neque accusamus, libero eius beatae nostrum quae quasi provident dignissimos debitis est vel et non harum! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, natus.</div>
+            </div>
+        </div>
     </div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import IntroPage from './__components/IntroPage'
+import WhatWeDo from './__components/WhatWeDo'
 
 export default function page() {
   return (
     <div className=''>
       <IntroPage />
+      <WhatWeDo />
     </div>
   )
 }
